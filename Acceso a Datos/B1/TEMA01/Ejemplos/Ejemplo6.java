2e0e19e6c100c78157f6166f321365baf18d2df4
PepeJEJEJ <jah0005@alu.medac.es>
2026-09-24 12:39:04 +0200

    a
    fa

--- Acceso a Datos/B1/TEMA01/Ejemplos/Ejemplo6.java
+++ Acceso a Datos/B1/TEMA01/Ejemplos/Ejemplo6.java
@@ -5,8 +5,12 @@ import java.io.*;
 public class Ejemplo6 {
 
     public static void main(String[] args) {
-        RandomAccessFile file = new RandomAccessFile("./tema1/abecedatio.txt", null);
-        file.seek(5);
-        
+        try {
+            RandomAccessFile file = new RandomAccessFile("./tema1/abecedatio.txt", "r");
+            file.seek(5);
+            long puntero = file.getFilePointer();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
