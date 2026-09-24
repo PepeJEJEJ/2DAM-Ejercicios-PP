8b4770cdde9901ea80e6012d03fd022e55b27719
PepeJEJEJ <jah0005@alu.medac.es>
2026-09-24 12:53:39 +0200

    Update Ejemplo6.java

--- Acceso a Datos/B1/TEMA01/Ejemplos/Ejemplo6.java
+++ Acceso a Datos/B1/TEMA01/Ejemplos/Ejemplo6.java
@@ -6,12 +6,12 @@ public class Ejemplo6 {
 
     public static void main(String[] args) {
         try {
-            RandomAccessFile file = new RandomAccessFile("./tema1/abecedatio.txt", "r");
+            RandomAccessFile file = new RandomAccessFile("./tema1/abecedario.txt", "r");
             file.seek(5);
-            sout(puntero antes de leer + file.getFilePoiter());
-            int unbyte=file.read();
-            sout(puntero despues de leer + file.getFilePoiter());
-            sout(unbyte);
+            System.out.println("puntero antes de leer " + file.getFilePointer());
+            int unbyte = file.read();
+            System.out.println("puntero después de leer " + file.getFilePointer());
+            System.out.println(unbyte);
         } catch (Exception e) {
             e.printStackTrace();
         }
